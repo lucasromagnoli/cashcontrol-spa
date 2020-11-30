3c7a9f9c546aced380e8ccfc6f29beb39c884c8c
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-11-30 20:51:33 -0300

    Recriando o projeto

--- src/main.js
+++ src/main.js
@@ -1,25 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router';
-import vuetify from './plugins/vuetify';
-import VCurrencyField from 'v-currency-field';
-import { VTextField } from 'vuetify/lib'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
-Vue.component('v-text-field', VTextField)
 
-Vue.use(VCurrencyField, {
-  locale: 'pt-BR',
-  decimalLength: 2,
-  autoDecimalMode: true,
-  min: null,
-  max: null,
-  defaultValue: 0,
-  valueAsInteger: false,
-  allowNegative: false
-})
 new Vue({
   router,
-  vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
